3eab97ce25212da6b542124e681eb0a75dd0a225
Isabella <isabellarose.hernandez01@gmail.com>
2021-11-22 12:29:37 -0600

    adds strings and primitive types file

---
+++ strings.js
@@ -0,0 +1,5 @@
+var sentOne = "My name is Isabella but I go by Bella. ";
+var sentTwo = "\nI have a dog named Perry who is 9 years old. ";
+var sentThree = "\nMy favorite color is light blue.";
+
+console.log(sentOne + sentTwo + sentThree + "\nI am 20 years old.");
