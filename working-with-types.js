f7470c2b90b0389efc1452217f9e35074a137a2a
Isabella <isabellarose.hernandez01@gmail.com>
2021-11-22 16:18:38 -0600

    modifies working with types

--- working-with-types.js
+++ working-with-types.js
@@ -33,7 +33,21 @@ var sample = "Hello Codeup";
 
 //console.log(sample.length);
 //console.log(sample.toUpperCase());
-//console.log(sample.concat(" Students"));
+
+sample += " Students";
+
+sample = sample.replace("Students", "Class");
+//console.log(sample);
+
 //console.log(sample.indexOf("c"));
 //console.log(sample.indexOf("C"));
-//console.log(sample.substr(6, 12));
+
+console.log(sample.substring(sample.indexOf("C"), sample.indexOf("p") + 1));
+
+//Exercise 3
+//var x = 3;
+//var Mermaid = 3x;
+//var Bear = 5x;
+//var Hercules = 1x;
+
+//console.log();
