546df65dba358d017526884a630261f76306f029
Isabella <isabellarose.hernandez01@gmail.com>
2021-11-22 21:16:34 -0600

    finishes working with types

--- working-with-types.js
+++ working-with-types.js
@@ -42,12 +42,48 @@ sample = sample.replace("Students", "Class");
 //console.log(sample.indexOf("c"));
 //console.log(sample.indexOf("C"));
 
-console.log(sample.substring(sample.indexOf("C"), sample.indexOf("p") + 1));
+//console.log(sample.substring(sample.indexOf("C"), sample.indexOf("p") + 1));
 
 //Exercise 3
-//var x = 3;
-//var Mermaid = 3x;
-//var Bear = 5x;
-//var Hercules = 1x;
+var cost = 3;
+var Mermaid = 3 * cost;
+var Bear = 5 * cost;
+var Hercules = 1 * cost;
 
-//console.log();
+//console.log(Mermaid + Bear + Hercules);
+
+var google = 400;
+var amazon = 380;
+var facebook = 350;
+
+var gt = google * 6;
+var at = amazon * 4;
+var ft = facebook * 10;
+
+//console.log(gt + at + ft);
+
+var full = false;
+var conflict = false;
+
+var studentEnroll = !full && !conflict;
+
+//console.log(studentEnroll);
+
+var expired = false;
+var twoItemsPlus = true;
+
+var customer = !expired && !twoItemsPlus;
+var preCustomer = expired;
+
+//console.log(customer);
+
+//Exercise 4
+
+var passFivePlus = true;
+var passNotUser = true;
+var userNotTwenty = true;
+var noWhitespace = true;
+
+//console.log(passFivePlus && passNotUser && userNotTwenty && noWhitespace);
+
+
