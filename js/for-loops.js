4c954a0759d9e39aca4b0f93ae56fd80c90676d3
Isabella <isabellarose.hernandez01@gmail.com>
2021-12-02 16:58:57 -0600

    created new files and solved problems in for-loops

---
+++ js/for-loops.js
@@ -0,0 +1,37 @@
+// problem 1
+// function showMultiplicationTable(number) {
+//     for(let i = 1; i <= 10; i++) {
+//         console.log(number + " x " + i + " = " + (i * number));
+//     }
+// }
+// showMultiplicationTable(5);
+
+//problem 2
+// for(let i = 0; i < 10; i++) {
+//     let randomNum = Math.floor(Math.random() * 181) + 20; //
+//         if(randomNum % 2 === 0) {
+//             console.log(randomNum + " is even");
+//         } else {
+//             console.log(randomNum + " is odd");
+//         }
+// }
+
+//problem 3
+// function generatePyramid() {
+//     let output = '';
+//     for (let i = 1; i <= 9; i++) {
+//         console.log(i);
+//         for (let j = 1; j <= i; j++) {
+//             output += i;
+//         }
+//         console.log(output);
+//         output = '';
+//     }
+// }
+//
+// generatePyramid();
+
+//problem 4
+// for(let i = 100; i >= 5; i -= 5) {
+//     console.log(i);
+// }
