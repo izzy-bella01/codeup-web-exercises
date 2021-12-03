51220a14a45f2bfe286b10b7fb98067e4d2eb78a
Isabella <isabellarose.hernandez01@gmail.com>
2021-12-03 10:01:44 -0600

    completed while exercise

--- js/while.js
+++ js/while.js
@@ -2,6 +2,23 @@ let i = 2;
 
 while(i <= 65536) {
     console.log(i)
-    i = i * 2;
+    i *= 2;
 }
 
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+   var conesToPurchase = Math.floor(Math.random() * 5) + 1;
+   if(conesToPurchase <= allCones) {
+       console.log(conesToPurchase + " cones sold...");
+       allCones -= conesToPurchase;
+   } else {
+       console.log("Cannot sell you " + conesToPurchase + " I only have " + allCones + "...");
+   }
+
+   console.log(allCones)
+
+}while(allCones > 0)
+
+console.log("Yay! I sold them all!")
