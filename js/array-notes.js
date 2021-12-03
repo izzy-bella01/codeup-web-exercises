9168b31141112ab65f3498232e7c613eb3486c15
Isabella <isabellarose.hernandez01@gmail.com>
2021-12-03 13:52:35 -0600

    created array nots & iterating js files. completed iterating js

---
+++ js/array-notes.js
@@ -0,0 +1,76 @@
+//let students = ["Gerald", "Mark", "Mina", "Jason", "Justin"];
+// console.log(students.length)
+// console.log(students[3]);
+// console.log(students[5]);
+
+// concat the last name to the first name
+// access Justin's value
+// re-assign the value to equal justin + last name
+// console.log(students[4]);
+// students[4] = students[4] + " Sebastian" // or students[4] += "Sebastian"
+// console.log(students[4]);
+
+
+// let favorites = ["Pizza🍕", "Mac & Cheese🍝", "Waffles🧇", "Ice Cream🍦", "Chicken Wings🍗"];
+// console.log(favorites);
+//
+// favorites[0] += " 1"
+// favorites[1] += " 2"
+// favorites[2] += " 5"
+// favorites[3] += " 3"
+// favorites[4] += " 4"
+//
+// console.log(favorites);
+//
+// let students = ["Gerald", "Mark", "Mina", "Jason", "Mina"];
+// let indexOfMina = students.indexOf("Mina")
+// console.log(indexOfMina);
+// let otherIndexOfMina = students.lastIndexOf("Mina")
+// console.log(otherIndexOfMina);
+
+//ACCESSING Elements from AN ARRAY
+//nameOfArray[position within array] 💡 arrays always start at zero
+//accessing arrays returns the value at that position
+//     let valIndexZero = students[0];
+//     console.log(valIndexZero);
+//
+//
+//     let faveAnimals = ["dog", "cat", "elephant"]
+//     let laura = ["Laura", 31, "San Antonio", "MAC", ["Pizza", "Mac & Cheese", "Ice Cream"], 2021, faveAnimals]
+//     let age = laura[1]
+//     console.log(age)
+//     let innerArray = laura[4];
+//     console.log(innerArray);
+//     console.log(innerArray[2]);
+//     console.log(laura[4][1]);
+//     console.log(laura[6]);
+//     console.log(laura);
+//
+//     function desiredAge(num) {
+//         return num - 10
+//     }
+//
+//     console.log(desiredAge(age));
+
+
+//FUNCTIONS
+
+// function nameOfFunction(num1, randomFunction){
+//     randomFunction();
+//     return num1;
+// }
+// console.log(nameOfFunction(4, function () {
+//     console.log("hey there!")
+// }));
+
+//METHODS
+//methods are pre-existing functions
+// Math.random() // generate random number between 0 and 1
+// console.log("print this out");
+// parseInt();
+//
+// let ages = [14, 23, 89, 5, 25, 36];
+// for(let i = 0; i < ages.length; i++) {
+//     console.log(ages[i] * 2);
+// }
+
