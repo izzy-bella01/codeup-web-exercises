83131a034cb574b2f49a43697c30bb16f3c6b607
Isabella <isabellarose.hernandez01@gmail.com>
2021-12-07 15:42:00 -0600

    finished circles js

---
+++ js/circle.js
@@ -0,0 +1,48 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^
+
+            return Math.PI * (this.radius ** 2); // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            let areaValue;
+
+            if(doRounding) {
+                areaValue = Math.round(this.getArea());
+            } else {
+                areaValue = this.getArea()
+            }
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + areaValue);
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5;
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
