f160225bb954e1d022bd8a24e8e2236690f4b4cb
Isabella <isabellarose.hernandez01@gmail.com>
2022-01-13 10:28:03 -0600

    created dom event notes

---
+++ js/dom-event-notes.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// // ADD EVENT LISTENER
+//
+// // create a handler function
+// var listener = function (event) {
+//     alert('You clicked the button!');
+// }
+//
+// // register the listener to handle clicks on btn1
+// document.getElementById('btn1').addEventListener('click', listener);
+//
+//
+// // REMOVE EVENT LISTENER
+//
+// // var listener = function (event) {
+// //     alert('You clicked the button!');
+// // }
+//
+// // add event listener to btn2
+// var btn2 = document.getElementById('btn2');
+// btn2.addEventListener('click', listener);
+//
+// var remover = function (event) {
+//     // remove event listener from btn1
+//     btn2.removeEventListener('click', listener);
+//     console.log('Event listener removed.');
+// }
+//
+// // add event listener to btn3
+// var btn3 = document.getElementById('btn3');
+// btn3.addEventListener('click', remover);
+
+// NOTES IN CLASS
