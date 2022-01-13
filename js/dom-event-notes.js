16937e8114750d94b8020a9dfd238c7e3aebafe0
Isabella <isabellarose.hernandez01@gmail.com>
2022-01-13 11:37:33 -0600

    added more notes for DOM events

--- js/dom-event-notes.js
+++ js/dom-event-notes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// // ADD EVENT LISTENER
+// // REGISTER EVENT LISTENER
 //
 // // create a handler function
 // var listener = function (event) {
@@ -32,3 +32,39 @@
 // btn3.addEventListener('click', remover);
 
 // NOTES IN CLASS
+
+// // CHANGE TABLE OF CONTENTS TO ORANGE
+var el = document.getElementById("orange");
+// el.style.color = "orange";
+
+var listener = function () {
+    el.style.backgroundColor = "orange";
+}
+el.addEventListener("click", listener);
+
+
+// EVENT LISTENER USING BTN (CHANGE "HELLO WORLD" TO PINK WHEN CLICK ON BUTTON)
+
+// define hello world element
+// define button that will be listening for the click
+// add the event listener to the button (.addEventListener())
+// define what event is being listened for
+// change color of hello world (style.color = "pink")
+
+var helloWorldEl = document.getElementsByTagName("h1");
+console.log(helloWorldEl[0]);
+
+var btn = document.getElementById("btn");
+console.log(btn);
+
+var listener2 = function () {
+    helloWorldEl.style.color = "pink";
+}
+
+btn.addEventListener("click", listener2);
+
+// DARK MODE
+
+document.getElementById("dark-mode").addEventListener("click", function() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+})
