83dd795565045d056cbd30f0673e1364bf22a84d
Isabella <isabellarose.hernandez01@gmail.com>
2022-01-14 09:20:06 -0600

    added to warmup js file for january 14.

--- js/warmup.js
+++ js/warmup.js
@@ -65,77 +65,87 @@
 // console.log(remFun(45,7));
 
 //Warm up for 11-29-2021
-
-function reverseString(str) {
-    return str.split("").reverse().join("");
-}
+//
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
 //console.log(reverseString("hello"));
 
 //Warm up for 11-30-2021
 
 //Write a function that accepts the top three bowling scores and returns the average.
 
-function bowlingScores(topOne, topTwo, topThree) {
-    return ((topOne + topTwo + topThree) / 3).toFixed();
-}
-bowlingScores();
-console.log(bowlingScores(172, 160, 158));
+// function bowlingScores(topOne, topTwo, topThree) {
+//     return ((topOne + topTwo + topThree) / 3).toFixed();
+// }
+// bowlingScores();
+// console.log(bowlingScores(172, 160, 158));
 
 //Write a function that accepts the best score and returns true if its divisible by 3 or 5 and false if it’s not
 
-function bestScore(theTopScore) {
-    if (theTopScore % 3 === 0 || theTopScore % 5 === 0) {
-        return true;
-    } else {}
-    return false;
-}
-bestScore();
-console.log(bestScore(140));
+// function bestScore(theTopScore) {
+//     if (theTopScore % 3 === 0 || theTopScore % 5 === 0) {
+//         return true;
+//     } else {}
+//     return false;
+// }
+// bestScore();
+// console.log(bestScore(140));
 
 //Write a function that accepts the name of the bowling place (“ThEbEsTbOwLiNgPlAcEever”) and returns it in all lower case BONUS: return it with a space between each word
-let bowlingName = "ThEbEsTbOwLiNgPlAcEever";
-
-function bowlingPlace (pinName) {
-    return (pinName.toLowerCase());
-}
-
-bowlingPlace(bowlingName);
-console.log(bowlingPlace(bowlingName));
+// let bowlingName = "ThEbEsTbOwLiNgPlAcEever";
+//
+// function bowlingPlace (pinName) {
+//     return (pinName.toLowerCase());
+// }
+//
+// bowlingPlace(bowlingName);
+// console.log(bowlingPlace(bowlingName));
 
 //Given these variables: let score = 109; let name = “bowling Is Fun”; let isBowlingFun = true; let players = [“tom”, “jerry”, “Garfield”]; Write a function that returns the type of each of the variables (call the function once per variable);
-
-let score = 109;
-let name = "bowling Is Fun";
-let isBowlingFun = true;
-let players = ["tom", "jerry", "Garfield"];
-
-function whatIsTheType (varToBeTested) {
-    return typeof varToBeTested;
-}
-
-console.log(whatIsTheType(score));
-console.log(whatIsTheType(name));
-console.log(whatIsTheType(isBowlingFun));
-console.log(whatIsTheType(players));
+//
+// let score = 109;
+// let name = "bowling Is Fun";
+// let isBowlingFun = true;
+// let players = ["tom", "jerry", "Garfield"];
+//
+// function whatIsTheType (varToBeTested) {
+//     return typeof varToBeTested;
+// }
+//
+// console.log(whatIsTheType(score));
+// console.log(whatIsTheType(name));
+// console.log(whatIsTheType(isBowlingFun));
+// console.log(whatIsTheType(players));
 
 //Write a function that executes all of the above functions
 
 
 //Copy and execute the following functions, and find the Error: (Hint: try console logging the result)
 
-function myFunction (firstName, lastName) {
-    return firstName + " " + lastName;
-}
+// function myFunction (firstName, lastName) {
+//     return firstName + " " + lastName;
+// }
+//
+// myFunction();
+// console.log(myFunction("Isabella", "Hernandez"))
+//
+// function thatsNotMyName (myName) {
+//     if (myName === "laura") {
+//         return "that's me! I'm " + myName;
+//     } else {
+//         return "that's not my name";
+//     }
+// }
+// thatsNotMyName();
+// console.log(thatsNotMyName("laura"));
 
-myFunction();
-console.log(myFunction("Isabella", "Hernandez"))
 
-function thatsNotMyName (myName) {
-    if (myName === "laura") {
-        return "that's me! I'm " + myName;
-    } else {
-        return "that's not my name";
-    }
-}
-thatsNotMyName();
-console.log(thatsNotMyName("laura"));
+// WARMUP FOR 01-14-2022
+
+// Make a program that filters through this array: ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"] and returns a new array that contains only those whose name have only 4 letters in them.
+
+var names = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
+
+var newNames = names.filter(name => name.length === 4)
+console.log(newNames);
