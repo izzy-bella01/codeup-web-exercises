2283733e7c589a2daf5947b3a98b008a7ca5651e
Isabella <isabellarose.hernandez01@gmail.com>
2022-01-19 11:53:15 -0600

    continued jquery intro notes

--- js/jquery-intro.js
+++ js/jquery-intro.js
@@ -3,10 +3,35 @@
 document.getElementById("heading"); //DOM
 
 
-//jQuery
+// jQuery
 
 let myEl = $("#p-one")
 console.log(myEl);
 
 let heading = $("#heading");
 console.log(heading);
+
+// add a class of "border" to p tag
+
+$("#p-one").addClass("border");
+
+// add a border to the heading if the user is logged in:
+// check value of isLoggedIn
+// grab the value of isLoggedIn
+// save that value to a variable
+// compare value of logged in to true
+// add a border
+// assign a class to the element, if true
+// find the element
+
+let user1 = {
+    name: "laura",
+    isLoggedIn: true,
+    age: 31
+}
+
+let loginStatus = user1.isLoggedIn;
+
+if(loginStatus === true) {
+    $("#heading").addClass("border") // $("#heading") -- > replaces document.getElementById
+}
