a5c3777e6f4a3053c43067f39c9154ef9b61274e
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-04 10:29:13 -0600

    created es6.js file for exercise

---
+++ js/es6-notes.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var x = 2 ** 8;
+
+const MY_CONST = 1;
+
+// if(true) {
+//     let test1 = 'bob';
+// }
+// console.log(test1);
+
+// TEMPLATE STRINGS
+
+// const name = 'codeup';
+// console.log('Hello, ' + name + '!');
+// // same as
+// console.log(`Hello, ${name}!`);
+
+// const name = 'codeup';
+// console.log(`Hello, ${name.toUpperCase()}!`); // Hello, CODEUP!
+
+// FOR OF
+
+// let fruits = [
+//     {
+//         name: 'apple',
+//         qty: 4
+//     },
+//     {
+//         name: 'kiwi',
+//         qty: 6
+//     },
+//     {
+//         name: 'banana',
+//         qty: 3
+//     },
+//     {
+//         name: 'pineapple',
+//         qty: 1
+//     }
+// ]
+//
+// for (const fruit of fruits) {
+//     console.log(fruit);
+// }
+
+// ARROW FUNCTIONS
+
+// function notArrow(x) {
+//     return x + '!';
+// }
+// console.log(notArrow("timothy"));
+//
+// const arrow = (x) => {
+//     return 'i am ' + x;
+// }
+// console.log(arrow("bella"));
