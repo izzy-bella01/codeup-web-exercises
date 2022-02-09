a54dff579dc32c95a5952f4ced23c2a80fd0ef89
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-09 11:34:20 -0600

    finished notes for restful api

--- js/restful-api-notes.js
+++ js/restful-api-notes.js
@@ -2,4 +2,90 @@ console.log("---------------------------------");
 console.log("RESTful API Requests");
 console.log("---------------------------------");
 
+// HEADERS:
+// fetch("api/url", {
+//     'Content-Type': "application/json"
+// })
 
+// METHOD TYPE:
+// GET, POST, PUT, DELETE, PATCH
+// GET - getting information (reading)
+    // what's required when making a GET request?
+        // - url
+        // - api token (might be needed for all anyways)
+        // - sometimes an id of something
+    // WHAT WILL THE RESPONSE BE????
+        // - response object => response.json()
+        // {"id": 4, "username":"Bella}
+
+// POST - adding information / pushing data into the API (writing)
+    // access key(headers)
+    // request body
+        // data to be added
+    // what's required when making a POST request?
+        // - url
+        // - what we're adding - JSON OBJECT
+    // WHAT WILL THE RESPONSE BE????
+        // - confirmation is not guaranteed
+        // - response object --> res => res.json() --> res.status || res.ok
+        // - err if err
+            // how can we confirm if posted successfully
+                // we can either make a second request after the post request made
+                // - we can look into the data/api itself --> if you have access to it
+
+// PUT - changes something that's already on the API (editing)
+    // what should be sent with a PUT request?
+        // - identify the data you're changing
+        // - what you're changing it to -- JSON OBJECT
+    // WHAT WILL THE RESPONSE BE????
+        // - dont get actual???????
+
+// DELETE - delete a specific data from the API
+    // what's required when making a DELETE request?
+        // - id of the thing we're deleting
+    // WHAT WILL THE RESPONSE BE????
+        //???????
+
+// POST
+
+// url
+// content being added - json
+// token ???
+// endpoint ???
+// method type - specify that we're making a POST request
+
+// const url = 'https://codeup-restful-example.glitch.me/reviews';
+// const reqBody = {
+//     "id": 248,
+//     "restaurant_id": 1,
+//     "name": "Bella",
+//     "createdAt": 1504095567183,
+//     "updatedAt": 1504095567183,
+//     "rating": "5",
+//     "comments": "BELLA WAS HERE"
+// }
+// const options  = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(reqBody)
+// };
+//
+// fetch(url, options)
+//     .then(res => res.json())
+//     .then(jsonObj => console.log(jsonObj))
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'Bella',
+        body: 'Was Here :)',
+        userId: 1,
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json));
