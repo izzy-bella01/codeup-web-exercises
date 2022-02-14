6f433634b81ea934dc76eae7ef950f25b7a5287a
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-14 16:21:43 -0600

    made scratch file for project

---
+++ js/movie-project-scratch.js
@@ -0,0 +1,92 @@
+// {
+//     "movies": [
+//     {
+//         "title": "Tenet",
+//         "rating": "5",
+//         "poster": "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
+//         "year": "2020",
+//         "genre": "Action, Sci-Fi",
+//         "director": "Christopher Nolan",
+//         "plot": "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
+//         "actors": "Elizabeth Debicki, Robert Pattinson, John David Washington, Aaron Taylor-Johnson",
+//         "id": 1
+//     },
+//     {
+//         "title": "Titanic",
+//         "rating": "5",
+//         "poster": "https://cdn.shopify.com/s/files/1/0747/3829/products/mHP0043_1024x1024.jpeg?v=1571444279",
+//         "year": "1997",
+//         "genre": "Romance, Drama",
+//         "director": "Paul W.S. Anderson",
+//         "plot": "James Cameron's 'Titanic' is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built. She was the most luxurious liner of her era -- the 'ship of dreams' -- which ultimately carried over 1,500 people to their death in the ice cold waters of the North Atlantic in the early hours of April 15, 1912.",
+//         "actors": "Ryan McCluskey, Oscar Pearce, Indra Ové, Anna Bolt",
+//         "id": 3
+//     },
+//     {
+//         "title": "The Princess Bride",
+//         "rating": "5",
+//         "poster": "https://lumiere-a.akamaihd.net/v1/images/p_princessbride_19348_769a80b9.jpeg",
+//         "year": "1987",
+//         "genre": "Romance, Comedy, Adventure",
+//         "director": "Rob Reiner",
+//         "plot": "A fairy tale adventure about a beautiful young woman and her one true love. He must find her after a long separation and save her. They must battle the evils of the mythical kingdom of Florin to be reunited with each other. Based on the William Goldman novel 'The Princess Bride' which earned its own loyal audience.",
+//         "actors": "Cary Elwes, Mandy Patinkin, Robin Wright",
+//         "id": 4
+//     },
+//     {
+//         "title": "Scream",
+//         "rating": "5",
+//         "poster": "https://m.media-amazon.com/images/I/7114ViRptyL._AC_.jpg",
+//         "year": "1996",
+//         "genre": "Horror, Mystery",
+//         "director": "Wes Craven",
+//         "plot": "Wes Craven re-invented and revitalised the slasher-horror genre with this modern horror classic, which manages to be funny, clever and scary, as a fright-masked knife maniac stalks high-school students in middle-class suburbia. Craven is happy to provide both tension and self-parody as the body count mounts - but the victims aren't always the ones you'd expect.",
+//         "actors": " David Arquette, Courteney Cox, Matthew Lillard, Rose McGowan, Drew Barrymore",
+//         "id": 5
+//     },
+//     {
+//         "title": "Monsters Inc.",
+//         "rating": "5",
+//         "poster": "https://www.themoviedb.org/t/p/original/gtQ63PJ2VIUmrqDJQMsgU4z8u99.jpg",
+//         "year": "2001",
+//         "genre": "Comedy, Fantasy",
+//         "director": "Pete Docter",
+//         "plot": "Monsters Incorporated is the largest scare factory in the monster world, and James P. Sullivan (John Goodman) is one of its top scarers. Sullivan is a huge, intimidating monster with blue fur, large purple spots and horns. His scare assistant, best friend and roommate is Mike Wazowski (Billy Crystal), a green, opinionated, feisty little one-eyed monster. Visiting from the human world is Boo (Mary Gibbs), a tiny girl who goes where no human has ever gone before.",
+//         "actors": "Billy Crystal, John Goodman, Mary Gibbs",
+//         "id": 6
+//     },
+//     {
+//         "title": "Monsters Inc.",
+//         "rating": "5",
+//         "poster": "https://www.themoviedb.org/t/p/original/gtQ63PJ2VIUmrqDJQMsgU4z8u99.jpg",
+//         "year": "2001",
+//         "genre": "Comedy, Fantasy",
+//         "director": "Pete Docter",
+//         "plot": "Monsters Incorporated is the largest scare factory in the monster world, and James P. Sullivan (John Goodman) is one of its top scarers. Sullivan is a huge, intimidating monster with blue fur, large purple spots and horns. His scare assistant, best friend and roommate is Mike Wazowski (Billy Crystal), a green, opinionated, feisty little one-eyed monster. Visiting from the human world is Boo (Mary Gibbs), a tiny girl who goes where no human has ever gone before.",
+//         "actors": "Billy Crystal, John Goodman, Mary Gibbs",
+//         "id": 7
+//     },
+//     {
+//         "title": "Monsters Inc.",
+//         "rating": "5",
+//         "poster": "https://www.themoviedb.org/t/p/original/gtQ63PJ2VIUmrqDJQMsgU4z8u99.jpg",
+//         "year": "2001",
+//         "genre": "Comedy, Fantasy",
+//         "director": "Pete Docter",
+//         "plot": "Monsters Incorporated is the largest scare factory in the monster world, and James P. Sullivan (John Goodman) is one of its top scarers. Sullivan is a huge, intimidating monster with blue fur, large purple spots and horns. His scare assistant, best friend and roommate is Mike Wazowski (Billy Crystal), a green, opinionated, feisty little one-eyed monster. Visiting from the human world is Boo (Mary Gibbs), a tiny girl who goes where no human has ever gone before.",
+//         "actors": "Billy Crystal, John Goodman, Mary Gibbs",
+//         "id": 8
+//     },
+//     {
+//         "title": "Monsters Inc.",
+//         "rating": "5",
+//         "poster": "https://www.themoviedb.org/t/p/original/gtQ63PJ2VIUmrqDJQMsgU4z8u99.jpg",
+//         "year": "2001",
+//         "genre": "Comedy, Fantasy",
+//         "director": "Pete Docter",
+//         "plot": "Monsters Incorporated is the largest scare factory in the monster world, and James P. Sullivan (John Goodman) is one of its top scarers. Sullivan is a huge, intimidating monster with blue fur, large purple spots and horns. His scare assistant, best friend and roommate is Mike Wazowski (Billy Crystal), a green, opinionated, feisty little one-eyed monster. Visiting from the human world is Boo (Mary Gibbs), a tiny girl who goes where no human has ever gone before.",
+//         "actors": "Billy Crystal, John Goodman, Mary Gibbs",
+//         "id": 9
+//     }
+// ]
+// }
